1b6c5ec55f13b4a9cd3a4c58a40dbe753c55cf7a
godbin <arashi0121@naver.com>
2025-04-11 08:43:19 +0900

    2525.js 푸는 중

--- Notes/2) Baekjoon/problems/2525.js
+++ Notes/2) Baekjoon/problems/2525.js
@@ -2,11 +2,23 @@ let fs = require('fs')
 let input = fs.readFileSync('../inputs/2525.txt').toString().split('\n')
 
 let now = input[0].split(' ')
-let hour = now[0]
-let min = now[1]
-let timer = input[1]
+let hour = parseInt(now[0])
+let min = parseInt(now[1])
+let timer = parseInt(input[1])
 
-console.log(input[0].split(' '))
-console.log(input[1])
+let addHour = timer/60
+let addMin = timer%60
 
-console.log(timer/60)
+hour += parseInt(addHour)
+min += parseInt(addMin)
+
+if(min>=60){
+  hour = parseInt(min/60)
+  min = min%60
+}
+
+if(hour>=24){
+  hour = parseInt(hour%24)
+}
+
+console.log(`${hour} ${min}`)
