a7c2e8f48b29f5b1dc6e0cea70997eb42bf9ff40
godbin <arashi0121@naver.com>
2025-04-15 08:32:04 +0900

    solve: 2675

--- Notes/2) Baekjoon/problems/2675.js
+++ Notes/2) Baekjoon/problems/2675.js
@@ -3,22 +3,17 @@ const input = fs.readFileSync("../inputs/2675.txt").toString().trim().split("\n"
 
 function solution(arr) {
   arr.shift();
-  let result = "";
-  arr.forEach((item)=>{
-    let num = item.split(' ')[0];
-    let str = item.split(' ')[1];
-    result = repeat(num,str) + '\n';
-  })
-  return result;
-}
-
-function repeat(num, str) {
-  let returnValue = "";
-  for(x in str){
-    for(let i=0;i<num;i++){
-      returnValue += x;
+  let txt = "";
+  arr.forEach((item) => {
+    let info = item.split(' ');
+    for (x of info[1]) {
+      for (let i = 0; i < info[0]; i++) {
+        txt += x
+      }
     }
-  }
+    txt += '\n';
+  })
+  console.log(txt);
 }
 
-console.log(solution(input));
+solution(input);
