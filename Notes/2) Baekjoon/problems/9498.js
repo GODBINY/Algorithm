921fdba5e8217f4bd080feae4d27db66d25a455d
godbin <arashi0121@naver.com>
2025-04-22 08:28:25 +0900

    chore: 디렉토리 정리

--- Notes/2) Baekjoon/problems/9498.js
+++
@@ -1,6 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync('../inputs/9498.txt').toString().split('/n');
-
-let grade = parseInt(input[0]);
-
-console.log(grade>89 ? 'A' : grade>79 ? 'B' : grade>69 ? 'C' : grade>59 ? 'D' : 'F' )
