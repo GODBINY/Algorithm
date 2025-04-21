65806a88edd8e7b5ee1493d7b010cefaea1a12d7
godbin <arashi0121@naver.com>
2025-04-22 08:38:39 +0900

    add: selection sort practice

---
+++ notes/2)sort/selection-sort.js
@@ -0,0 +1,18 @@
+let arr = [10, 3, 2, 8, 7, 6];
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIdx] > arr[j]) {
+        minIdx = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[minIdx];
+    arr[minIdx] = temp;
+  }
+}
+
+selectionSort(arr);
+console.log(arr);
