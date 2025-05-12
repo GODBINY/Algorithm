bac655e44bf909b1072ce0abd0189de0b8969610
godbin <arashi0121@naver.com>
2025-05-13 08:13:54 +0900

    solve 1181

--- practice/2) Baekjoon/problems/1181.js
+++ practice/2) Baekjoon/problems/1181.js
@@ -3,13 +3,22 @@ let input = fs.readFileSync('../inputs/1181.txt').toString().trim().split('\n');
 
 
 input.shift();
+const uniqueArr = [...new Set(input)].sort();
 
-input.sort((a, b) => {
-  if (a.length === b.length) {
-    return a - b;
-  }
+uniqueArr.sort((a, b) => {
   return a.length - b.length;
 })
 
-console.log(input);
+console.log(uniqueArr.join('\n'));
+
+
+// 나는 정렬을 두번 했는데, 정렬 함수를 커스텀해서 한번에 하는게 좋겠어.
+uniqueArr.sort((a, b) => {
+  if (a.length !== b.length) return a.length - b.length;
+  else {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
+  }
+})
 
