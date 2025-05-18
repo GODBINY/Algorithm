dc21162e8d7086751250a9fe0f1bdc1465b29fa7
godbin <arashi0121@naver.com>
2025-05-19 08:40:23 +0900

    solving 11047

--- practice/2) Baekjoon/problems/11047.js
+++ practice/2) Baekjoon/problems/11047.js
@@ -1,4 +1,25 @@
 const fs = require("fs");
 const input = fs.readFileSync("../inputs/11047.txt").toString().trim().split("\n");
 
+let totalMoney = input[0].split(' ')[1];
+console.log(input)
+input.shift();
+
+console.log(input)
+
+input.sort((a, b) => b - a);
+
+let count = 0;
+let remain = totalMoney;
+
+input.forEach(item => {
+  if (remain > 0) {
+    count = remain / item;
+    remain = remain % item;
+  }
+})
+
+console.log(count);
+console.log(remain);
+
 
