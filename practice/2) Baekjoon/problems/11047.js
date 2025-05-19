adcea93092105a2bf9883700e3172f4c01b97df2
GODBIN <blackbin21@daiso.co.kr>
2025-05-20 08:47:59 +0900

    fix:11047 ... 등호 하나 안넣어서 틀렸음... 앞으론 등호 유의하자 ㅠㅠ

--- practice/2) Baekjoon/problems/11047.js
+++ practice/2) Baekjoon/problems/11047.js
@@ -1,19 +1,24 @@
 const fs = require("fs");
-const input = fs.readFileSync("../inputs/11047.txt").toString().trim().split("\n");
+const input = fs
+  .readFileSync("../inputs/11047.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-let totalMoney = input[0].split(' ')[1];
+let type = input[0].split(" ")[0];
+let totalMoney = input[0].split(" ")[1];
 input.shift();
-
 input.sort((a, b) => b - a);
+input.map(Number);
 
+console.log(input);
 let count = 0;
 let remain = Number(totalMoney);
 
 for (i in input) {
   if (remain > 0 && remain > Number(input[i])) {
-    count = parseInt(count + (remain / input[i]));
+    count = parseInt(count + remain / input[i]);
     remain = remain % input[i];
   }
 }
 console.log(count);
-
