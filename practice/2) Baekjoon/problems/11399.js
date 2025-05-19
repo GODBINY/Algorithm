b1a57ece98861edd8e918f207f9edf1b211d4ffd
GODBIN <blackbin21@daiso.co.kr>
2025-05-20 08:57:48 +0900

    solving 11399

--- practice/2) Baekjoon/problems/11399.js
+++ practice/2) Baekjoon/problems/11399.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 const input = fs
-  .readFileSync("../inputs/11047.txt")
+  .readFileSync("../inputs/11399.txt")
   .toString()
   .trim()
   .split("\n");
+
+console.log(input);
+
+let people = input[1].split(" ").map(Number);
+
+console.log(people);
+
+// greedy 로 풀자면. 각 단계에서 시간 제일 적게 걸리는 사람을 불러모으면 되잖아?..
