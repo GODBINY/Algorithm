7d22431b4ef3f3d2e1236a2bffe021d7107c4d3d
GODBIN <blackbin21@daiso.co.kr>
2025-05-21 08:56:05 +0900

    solve 11399

--- practice/2) Baekjoon/problems/11399.js
+++ practice/2) Baekjoon/problems/11399.js
@@ -5,10 +5,18 @@ const input = fs
   .trim()
   .split("\n");
 
-console.log(input);
-
+let num = Number(input[0]);
 let people = input[1].split(" ").map(Number);
 
-console.log(people);
+people.sort((a, b) => a - b);
 
+let sum = 0;
+let answer = 0;
 // greedy 로 풀자면. 각 단계에서 시간 제일 적게 걸리는 사람을 불러모으면 되잖아?..
+
+for (let i = 0; i < num; i++) {
+  sum += Number(people[i]);
+  answer += sum;
+}
+
+console.log(answer);
