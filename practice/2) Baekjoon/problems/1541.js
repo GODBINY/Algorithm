546c85856c28a03a28706848afe5671b37afa400
godbin <arashi0121@naver.com>
2025-05-21 19:10:50 +0900

    solve 1541

--- practice/2) Baekjoon/problems/1541.js
+++ practice/2) Baekjoon/problems/1541.js
@@ -4,3 +4,16 @@ const input = fs
   .toString()
   .trim()
   .split("\n");
+
+// 음수가 가장 많아야한다.
+let group = input[0].split('-')
+console.log(group)
+
+let plusGroup = group.map((item) => {
+  return item.split('+').map(Number).reduce((acc, cur) => acc + cur, 0)
+})
+
+console.log(plusGroup)
+
+let answer = plusGroup.reduce((acc, cur) => acc - cur)
+console.log(answer)
