1ec4910d7153f90aa25a20b9420eef359d278112
godbin <arashi0121@naver.com>
2025-05-28 08:31:45 +0900

    solving 1946 - 답은 맞는데 메모리 초과...

--- practice/2) Baekjoon/problems/1946.js
+++ practice/2) Baekjoon/problems/1946.js
@@ -6,57 +6,44 @@ const input = fs
   .split("\n");
 
 input.shift();
-// 
-let cases = [];
-// 
-// input.forEach((item, idx) => {
-//   if (!item.includes(' ')) {
-//     cases.push(input.slice(idx + 1, idx + Number(item) + 1));
-//   }
-// })
-let idx = Number(input[0]);
-let count = 0;
-let tempArr = [];
 
-for (let i = 1; i < input.length; i++) {
-  if (count === idx) {
-    cases.push(input[i]);
-    tempArr = [];
-    idx = input[i];
+let result = [];
+let tempArr = [];
+let idx = 0;
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i].split(' ').length === 1) {
+    idx = Number(input[i]);
   } else {
-    tempArr.push(input[i]);
-    count++;
+    tempArr.push(input[i].split(' '));
+    if (tempArr.length === idx) {
+      result.push(tempArr);
+      tempArr = [];
+    }
   }
 }
+let sortedList = result.map((item, idx) => {
+  let sortedArr = [...item];
+  sortedArr.sort((a, b) => Number(a[0]) - Number(b[0]));
+  return sortedArr;
+})
 
-console.log(cases)
+// console.log(sortedList)
 
-// let test = cases.map((item) => {
-//   return item.map(e => e.split(' '))
-// })
-// 
-// let sortedList = test.map((item, idx) => {
-//   let sortedArr = [...item];
-//   sortedArr.sort((a, b) => Number(a[0]) - Number(b[0]));
-//   return sortedArr;
-// })
-// 
-// // console.log(sortedList)
-// 
-// let answer = [];
-// 
-// sortedList.forEach((item) => {
-//   let innerAnswer = 0;
-//   let min = 100001;
-// 
-//   for (let [x, y] of item) {
-//     if (y < min) {
-//       min = y;
-//       innerAnswer++;
-//     }
-//   }
-// 
-//   answer.push(innerAnswer);
-// })
-// 
-// console.log(answer.join('\n'))
+let answer = [];
+
+sortedList.forEach((item) => {
+  let innerAnswer = 0;
+  let min = 100001;
+
+  for (let [x, y] of item) {
+    if (y < min) {
+      min = y;
+      innerAnswer++;
+    }
+  }
+
+  answer.push(innerAnswer);
+})
+
+console.log(answer.join('\n'))
