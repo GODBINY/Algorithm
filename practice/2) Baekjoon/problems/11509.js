e83519e52815dd1a5608e18c4c4207fcb10cf6ee
godbin <arashi0121@naver.com>
2025-06-03 00:02:32 +0900

    solving 11509

--- practice/2) Baekjoon/problems/11509.js
+++ practice/2) Baekjoon/problems/11509.js
@@ -6,6 +6,30 @@ const input = fs
   .split("\n");
 
 console.log(input);
-input.shift();
-let arr = input;
+let num = Number(input[0]);
+let array = input[1].split(' ').map(Number);
+let arrowList = [];
+let result = 0;
 
+for (let i = 0; i < num; i++) {
+  console.log('i:', i)
+  if (i !== 0) {
+    for (let j = 0; j < arrowList.length; j++) {
+      if (arrowList[j] === array[i]) {
+        result++;
+        if (arrowList[j] >= 1) {
+          arrowList[j] = arrowList[j] - 1;
+        }
+      } else {
+        console.log(array[i])
+        arrowList.push(array[i] - 1);
+        result++;
+      }
+    }
+  } else {
+    arrowList.push(array[i] - 1);
+    result++;
+  }
+}
+
+console.log(result);
