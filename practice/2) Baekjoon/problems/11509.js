b68d094d74c3cafb89ba88300858394d1074e959
godbin <arashi0121@naver.com>
2025-06-06 17:19:16 +0900

    solve 9009

--- practice/2) Baekjoon/problems/11509.js
+++ practice/2) Baekjoon/problems/11509.js
@@ -5,30 +5,19 @@ const input = fs
   .trim()
   .split("\n");
 
-console.log(input);
 let num = Number(input[0]);
 let array = input[1].split(' ').map(Number);
-let arrowList = [];
+let arrowList = new Array(1000001).fill(0);
 let result = 0;
 
 for (let i = 0; i < num; i++) {
-  console.log('i:', i)
-  if (i !== 0) {
-    for (let j = 0; j < arrowList.length; j++) {
-      if (arrowList[j] === array[i]) {
-        result++;
-        if (arrowList[j] >= 1) {
-          arrowList[j] = arrowList[j] - 1;
-        }
-      } else {
-        console.log(array[i])
-        arrowList.push(array[i] - 1);
-        result++;
-      }
-    }
+  let height = array[i];
+  if (arrowList[height] > 0) {
+    arrowList[height] -= 1;
+    arrowList[height - 1] += 1;
   } else {
-    arrowList.push(array[i] - 1);
     result++;
+    arrowList[height - 1] += 1;
   }
 }
 
--- practice/2) Baekjoon/problems/9009.js
+++ practice/2) Baekjoon/problems/9009.js
@@ -6,10 +6,9 @@ const input = fs
   .split("\n")
   .map(Number);
 
-console.log(input);
-const num = input[0];
-
+let num = input[0];
 let arr = new Array(100).fill(0);
+let answer = [];
 
 for (let i = 0; i < arr.length; i++) {
   if (i <= 1) {
@@ -19,4 +18,32 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 
-console.log(arr);
+for (let i = 1; i <= num; i++) {
+  let arr = [];
+  cal(input[i], arr);
+  answer.push(arr)
+}
+
+function cal(number, orgArr) {
+  let n = 0;
+  let org = number;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= number) {
+      n = arr[i];
+    } else {
+      break;
+    }
+  }
+
+  if (n > 0) {
+    org -= n
+    orgArr.unshift(n);
+    cal(org, orgArr);
+  }
+}
+
+let ans = answer.map((item) => { return item.join(' ') }).join('\n');
+
+console.log(ans)
+
+
