0fd943378489049878a32a069adc3e94d3d9ac43
godbin <arashi0121@naver.com>
2025-06-09 07:51:05 +0900

    solving 19939

--- practice/2) Baekjoon/problems/19939.js
+++ practice/2) Baekjoon/problems/19939.js
@@ -4,6 +4,29 @@ const input = fs
   .toString()
   .trim()
   .split("\n")
-  .map(Number);
 
+const N = Number(input[0].split(' ')[0]);
+const K = Number(input[0].split(' ')[1]);
+let maxKList = [];
+let n = 1;
 
+let answer = 0;
+
+while (maxKList.length < 10000) {
+  for (let num = 0; num < n + 1; num++) {
+    maxKList.push(n);
+  }
+  n++;
+}
+
+if (K > maxKList[N - 1]) {
+  answer = -1;
+} else {
+  if (K % N === 0) {
+    answer = K / N;
+  } else {
+    answer = parseInt(K / N) + 1;
+  }
+}
+
+console.log(answer);
