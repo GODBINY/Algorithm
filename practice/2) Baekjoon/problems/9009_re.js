aee3f44f8d6280c8d00e2d7d9cadc9a035f3a457
godbin <arashi0121@naver.com>
2025-06-08 18:13:18 +0900

    solve 9009_re

--- practice/2) Baekjoon/problems/9009_re.js
+++ practice/2) Baekjoon/problems/9009_re.js
@@ -7,5 +7,29 @@ const input = fs
   .split("\n")
   .map(Number);
 
+// 피보나치 수열 만들기
+let fibo = [];
+fibo.push(0);
+fibo.push(1);
+while (fibo[fibo.length - 1] < 1e9) {
+  fibo.push(fibo[fibo.length - 2] + fibo[fibo.length - 1]);
+}
 
+let finalResult = [];
+
+for (let tc = 1; tc <= Number(input[0]); tc++) {
+  let n = Number(input[tc]);
+  let result = [];
+  let i = fibo.length - 1
+  while (n > 0) {
+    if (n >= fibo[i]) {
+      n -= fibo[i];
+      result.unshift(fibo[i]);
+    }
+    i--;
+  }
+  finalResult.push(result);
+}
+
+console.log(finalResult.map(item => item.join(' ')).join('\n'))
 
