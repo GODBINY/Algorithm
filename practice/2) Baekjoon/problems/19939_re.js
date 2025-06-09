8943eaac7fcbd889315d3aae5c75c8207f2d97ae
godbin <arashi0121@naver.com>
2025-06-10 00:08:39 +0900

    resolved 19939
    진짜 규칙성 스스로 다시 찾아서 풀었는데 틀림;;

---
+++ practice/2) Baekjoon/problems/19939_re.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("../inputs/19939.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let n = Number(input[0].split(' ')[0]);
+let k = Number(input[0].split(' ')[1]);
+
+// console.log(n, k);
+
+let limit = 0; // k가 있다면 n은 limit 이상이어야 한다.
+
+for (let num = 1; num <= k; num++) {
+  limit += num;
+}
+
+if (limit <= n) {
+  if (n % k !== 0) {
+    console.log(k)
+  } else {
+    console.log(k - 1);
+  }
+} else {
+  console.log(-1);
+}
+
+
