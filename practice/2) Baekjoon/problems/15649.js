872b9a0f10d7422666ccbe26135453ff54fa9932
godbin <arashi0121@naver.com>
2025-07-03 08:38:09 +0900

    example answer - 15649

---
+++ practice/2) Baekjoon/problems/15649.js
@@ -0,0 +1,29 @@
+let fs = require("fs");
+let input = fs.readFileSync("../inputs/15649.txt").toString().trim().split(" ");
+
+const N = Number(input[0]);
+const M = Number(input[1]);
+
+let path = [];
+let visited = new Array(N + 1).fill(false);
+let output = [];
+
+function dfs(depth) {
+  if (depth === M) {
+    output.push(path.join(" "));
+    return;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (visited[i]) continue;
+
+    visited[i] = true;
+    path.push(i);
+    dfs(depth + 1);
+    path.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+console.log(output.join("\n"));
