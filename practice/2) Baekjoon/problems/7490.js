cfaed5aed931837a9e8e15ca15c4ffb3f41b69e9
godbin <arashi0121@naver.com>
2025-07-04 07:41:01 +0900

    solving 7490

--- practice/2) Baekjoon/problems/7490.js
+++ practice/2) Baekjoon/problems/7490.js
@@ -10,32 +10,26 @@ console.log(input);
 const N = 3;
 let result = [];
 let cal = ['+', '-', ' '];
-let visited = [];
 let answer = [];
 
 function recursive(number) {
-  if (number === 1) {
-    result.push('1');
-  }
   if (calculate(result) === 0 && number == N + 1) {
     answer.push(result); // 한번에 모으던가 바로 출력하던가
     return;
   }
   for (let i = 0; i < 3; i++) { // 연산 반복
-    visited[i] = true;
     result.push(cal[i] + number);
     recursive(number + 1);
     result.pop()
   }
 }
 
-// recursive(1)
+recursive(1)
 
-// console.log(answer);
+console.log(answer);
 
 function calculate(orgArr) {
   let str = orgArr.join('').replaceAll(' ', '');
   return eval(str);
 }
 
-console.log(calculate(['1', '+', '2']));
