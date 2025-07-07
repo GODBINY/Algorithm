67082dedb2fdbc99b84ce8299baa3b3a61f84468
GODBIN <blackbin21@daiso.co.kr>
2025-07-07 17:06:16 +0900

    solving 7490

--- practice/2) Baekjoon/problems/7490.js
+++ practice/2) Baekjoon/problems/7490.js
@@ -1,47 +1,48 @@
 let fs = require("fs");
-let input = fs
-  .readFileSync("../inputs/7490.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let input = fs.readFileSync("../inputs/7490.txt").toString().trim().split("\n");
 
 console.log(input);
 let N = 0;
 let result = [];
-let cal = [' ', '+', '-'];
+let cal = [" ", "+", "-"];
 let answer = [];
 
-resultFunc(3)
+for (let i = 1; i <= input[0]; i++) {
+  // let totalResult = [];
+  // totalResult.push(resultFunc(Number(input[i])));
+  // totalResult.push('\n');
+  console.log(i);
+  console.log(resultFunc(Number(input[i])));
+}
 
 function resultFunc(number) {
-  N = number
-  recursive(1)
-  console.log(answer);
+  N = number;
+  recursive(1);
+  return answer;
 }
 
 function recursive(number) {
   if (number == N + 1) {
     if (calculate(result) === 0) {
-      answer.push(result.join('')); // 한번에 모으던가 바로 출력하던가
+      answer.push(result.join("")); // 한번에 모으던가 바로 출력하던가
     }
     return;
   }
   if (number == 1) {
     result.push(number);
     recursive(number + 1);
-    result.pop()
-
+    result.pop();
   } else {
-    for (let i = 0; i < 3; i++) { // 연산 반복
+    for (let i = 0; i < 3; i++) {
+      // 연산 반복
       result.push(cal[i] + number);
       recursive(number + 1);
-      result.pop()
+      result.pop();
     }
   }
 }
 
-
 function calculate(orgArr) {
-  let str = orgArr.join('').replaceAll(' ', '');
+  let str = orgArr.join("").replaceAll(" ", "");
   return eval(str);
 }
