c3f8fc26bc7d860e260a4000bba97d24da42e4bb
GODBIN <arashi0121@naver.com>
2025-09-05 19:42:20 +0900

    feat: solved 2529 (순열 BFS)

--- practice/2) Baekjoon/problems/2529.js
+++ practice/2) Baekjoon/problems/2529.js
@@ -6,9 +6,6 @@ const list = input[1].split(" ");
 let result = [];
 let visited = [];
 
-console.log(num);
-console.log(list);
-
 function dfs(current) {
   if (current.length === num + 1) {
     result.push([...current]);
@@ -16,8 +13,10 @@ function dfs(current) {
   }
 
   for (let i = 0; i <= 9; i++) {
+    if (visited[i]) continue;
+    // 이전 수 연산자 지금 수 했을 때 말 안되기 시작하면 continue로 넘겨버리도록 추가
+    if (current.length && !eval(`${current[current.length - 1]}${list[current.length - 1]}${i}`)) continue;
     if (!visited[i]) {
-      // 이전 수 연산자 지금 수 했을 때 말 안되기 시작하면 continue로 넘겨버리도록 추가
       visited[i] = true;
       current.push(i);
 
@@ -29,4 +28,4 @@ function dfs(current) {
   }
 }
 dfs([]);
-console.log(result);
+console.log(result[result.length - 1].join('') + '\n' + result[0].join(''));
