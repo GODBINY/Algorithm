584bc678adb1862ba6599105f6d25f3053a08517
GODBIN <arashi0121@naver.com>
2025-10-29 23:25:06 +0900

    feat: psudo code ㅊ추ㅜ가

--- practice/2) Baekjoon/problems/1012.js
+++ practice/2) Baekjoon/problems/1012.js
@@ -6,3 +6,11 @@ let input = fs
   .split("\n");
 
 console.log(input);
+
+// test case 수
+const testAmount = 0;
+// 가로,세로,배추수
+// 배추 수 만큼 위치 반복
+// let count = 0;
+// 기본 DFS 코드 작성 + 상하좌우 DFS
+// 전체 배추 위치 방문하면서 방문여부 체크 안되어있으면 DFS 수행 + count++
